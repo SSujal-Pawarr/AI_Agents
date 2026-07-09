bc38695a6bbf21bb9f83dbf67381b179063af5e5
SujalPawar <pawarsujal4455@gmail.com>
2026-07-09 22:11:00 +0530

    Modified  file

---
+++ email-agent/app/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
+MODEL = "openai/gpt-oss-20b:free"
+
+BASE_URL = "https://openrouter.ai/api/v1"
