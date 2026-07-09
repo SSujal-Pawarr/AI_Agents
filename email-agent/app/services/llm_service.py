9feb054db168233736eb37ef7bad7da0ff979736
SujalPawar <pawarsujal4455@gmail.com>
2026-07-09 22:10:19 +0530

    Modified  file

---
+++ email-agent/app/services/llm_service.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+
+from app.config import (
+    OPENROUTER_API_KEY,
+    MODEL,
+    BASE_URL,
+)
+
+
+class LLMService:
+
+    def __init__(self):
+
+        self.client = OpenAI(
+            api_key=OPENROUTER_API_KEY,
+            base_url=BASE_URL,
+        )
+
+    def generate(self, prompt: str) -> str:
+
+        response = self.client.chat.completions.create(
+            model=MODEL,
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+        )
+
+        return response.choices[0].message.content
