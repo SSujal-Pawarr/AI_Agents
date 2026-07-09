faebbd2634f834c3db0384303370e6fe48c92374
SujalPawar <pawarsujal4455@gmail.com>
2026-07-09 22:08:44 +0530

    Modified  file

---
+++ email-agent/app/models/email_response.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+from typing import Literal
+
+
+class EmailResponse(BaseModel):
+
+    category: Literal[
+        "Support",
+        "Sales",
+        "Billing",
+        "Refund",
+        "Technical Issue",
+        "Complaint",
+        "Feature Request",
+        "General Inquiry",
+        "Spam",
+    ]
+
+    priority: Literal[
+        "Low",
+        "Medium",
+        "High",
+    ]
+
+    needs_human: bool
+
+    reply: str
